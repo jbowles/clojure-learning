50e4329a5cc7ea61975c5ff470c4c966a091c7ff
joshua bowles <bowlesling@gmail.com>
2012-07-15 11:13:37 -0600

    more on non-lisp programmer tutorial and added json dep

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject cloj-learning "1.0.0-SNAPSHOT"
   :description "Learning clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.1.2"]])
--- src/cloj_learning/clojure_for_nonlisp_programmers.clj
+++ src/cloj_learning/clojure_for_nonlisp_programmers.clj
@@ -1,20 +1,105 @@
-;; doesn't work
-;(defn sysinfo []
-;  println((str "File system:" *file* "Compile Path:" *compile-path* "Version:" *clojure-version* )))
-;
-;(sysinfo)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       Clojure for non-Lisp Developers        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Some built-in keywords
+;;;;;;;; Basic Data Types
+;;;;;;;; http://clojure.org/data_structures
+
+;;list  IPersistentList
+;;(LISP = LISt Processing)
+;;;http://moxleystratton.com/clojure/clojure-tutorial-for-the-non-lisp-programmer
+;"Although the list can be used to perform operations, it can also be used to represent data. There is an important consequence of code and data sharing the same syntax: code can be manipulated as if it were data because it is data:" CaD.
+(list :foo (list 1 2 3) [4 5 6])
+;(:foo (1 2 3) [4 5 6])
+(class (list :foo (list 1 2 3) [4 5 6])) ;clojure.lang.PersistentList
+;
+'(:foo (list 1 2 3) [4 5 6])
+;(:foo (list 1 2 3) [4 5 6])
+;
+'(:foo '(1 2 3) [4 5 6])
+;(:foo (quote (1 2 3)) [4 5 6])
+
+;;vector  IPersistentVector
+;; the vector itself is the operator of the form: CaD.
+([7 8 9] 2)
+(class [7 8 9]) ;clojure.lang.PersistentVector
+
+;; map  IPersistentMap <hashed, sorted>
+;; the map can be used as a function of their keys: CaD.
+;; NOTE: keywords, numbers, strings --- applies to list and vector also
+;; NOTE: Can look (and behave?) similar to Rails HashWithIndifferentAccess
+{"a" 1 :b 2 :c :three, "d" "four"}
+(class {"a" 1 :b 2 :c :three, "d" "four"}) ;clojure.lang.PersistentArrayMap
+;; maintains orderd key-value
+;; Small hash maps will be created as ArrayMap by default --- linear lookup performance
+
+;; set  Set <hashed, sorted>
+;; for unique set of elements
+#{:a :b :c "d"}
+(class #{:a :b :c "d"}) ;clojure.langPersistentHashSet
+(hash-set :a :b :d :c)
+(sorted-set :a :b :d :c)
+(set [1 2 3 4 5 6])
+
+;; record  Record
+(defrecord User [fname lname address])
+;; ActiveRecord generally (Object.Model), instead namespace.Record
+(defrecord Address [street city state zip])
+(defrecord Foo [a b c])
+(class Foo)  ;java.lang.Class
+
+;; Create an instance of the User and Address records
+(def stu (User. "FirstName" "LastName"
+                  (Address. "300 N Street"
+                            "Somewhere"
+                            "Someplace"
+                            890876)))
+(:lname stu)
+(:fname stu)
+(:address stu)
+(-> stu :address :city)
+(-> stu :address :zip)
+(assoc stu :fname "Joshua")
+; "Joshua"
+(:fname stu)
+; "FirstName"
+(update-in stu [:address :zip] inc)
+; 89076
+(-> stu :address :zip)
+; 89077
+;; map from one record to another
+(def foo-instance (Foo. 10 20 30))
+(println foo-instance)
+(def foo-mapped-user (map->User (merge foo-instance {:fname nil})))
+(println foo-mapped-user)
+(def foo-mapped-user-two (map->User (merge foo-instance {:stu (:lname stu)})))
+(println foo-mapped-user-two)
+
+
+;; some other basic data types and their classes
+;; Keyword, String, Long,Boolean, Character, nil
+(class :a)      ;clojure.lang.Keyword
+(class *file*)  ;java.lang.String
+(class 3)       ;java.lang.Long
+(class "")      ;java.lang.String
+(class nil)     ;nil
+(class false)   ;java.lang.Boolean
+(class \n)      ;java.lang.Character
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Some built-in symbols
 ;*file*
 ;*compile-path*
 ;*clojure-version*
 ;*command-line-args*
-;+
+;+i
+;
+
 
 
 (defn
   ^{:limited true
-    :description "collect system information"
+    :description "collect system information using some of clojure's built-in symbols"
     :test (fn []
             (assert (or
                       (= 
@@ -29,6 +114,7 @@
   []
   [*file* *compile-path* *clojure-version*])
 
+;; get data about the function
 (sys-info)
 (.invoke sys-info) ; IFn interface uses this to get type checks to the JVM:: all arguments to .invoke are defined by IFn to be type Object. (http://kotka.de/blog/2012/06/Did_you_know_IX.html). 
 (source sys-info)
@@ -37,6 +123,9 @@
 (get (meta #'sys-info) :todo)
 (get (meta #'sys-info) :type)
 (if (:todo (meta #'sys-info)) (get (meta (var sys-info)) :todo))
+(doc sys-info)
+
+;; get data from the function
 (first (sys-info))
 (rest (sys-info))
 (last (sys-info))
@@ -96,7 +185,6 @@
 (source class)
 ;; we can only peek in so far...
 (println (str "This will error out " '(source ((mixed-maps) "string-key"))))
-
 (fn? ((mixed-maps) "string-key"))
 
 ;; let's get back to querying our data
@@ -149,4 +237,83 @@
 
 (around-zero 1.1 "negative" "zero" "positive")
 (macroexpand-1 '(around-zero 1.1 "negative" "zero" "positive"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;           something
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; defining a basic function
+(def function-syntax {:function "defn" :function-name "my-func" :vector-argument-list "[]" :argument-expressions "(...)"})
+
+
+(println (str function-syntax "\n(defn add-numbers [num1 num2] \n\t(+ num1 num2))") "\n\n\t example of function use... \n\n" `(add-numbers 5 4))
+
+;; anonymous function
+(fn [n1 n2] (+ n1 n1))
+((fn [n1 n2] (+ n1 n2)) 1 2)
+
+;; symbol bound to anonymous function
+(def add-two ((fn [n1 n2] (+ n1 n2)) 1 2))
+(println add-two)
+
+
+;; iterating
+;; loop
+(loop [i 0]
+  (when (< i 5)
+    (println "i:" i)
+    (recur (inc i))))
+
+;; dorun
+(dorun (for [i (range 0 5)]
+             (println i)))
+
+;; doseq
+(doseq [i (range 0 5)]
+  (println i))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     SEQUENCES ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Sequence  iSeq
+;; is an interface to a data type, not a data type
+(seq [1 2 3])
+;; we have defined a sequence (interface) to the the vector
+(class (seq [1 2 3])) ;clojure.lang.PersistentVector$ChunkedSeq  -> (.getClass (seq [1 2 3]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;    Java ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(new java.util.Date) ;java.util.Date
+(java.util.Date.) ;notice dot at the end
+(println (str "this is an error " '(Date.))) ;java.util.Date
+(new StringBuffer "A string buffer created") ;java.lang.StringBuffer
+(new java.lang.StringBuffer "A string buffer created again") ;java.lang.StringBuffer
+(StringBuffer. "A string buffer created again and again") ;java.lang.StringBuffer
+;; (.<method>) for method instance
+(.toString (new java.util.Date))
+(.toString (java.util.Date.))
+
+;; STATIC class fields and methods
+(Integer/MAX_VALUE)
+(Character/TYPE)
+(Boolean/valueOf "true")
+
+`(import '(java.io File FileReader)) ;multiple classes imported from package
+`(import '(java.io File FileReader) '(java.util HashMap)) ;multiple classes imported from multiple package
+`(FileReader. "text.txt")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;    Clojure Libraries ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use '[clojure.data.json :only (read-json json-str)])
+(json-str {:color "red" :name "apple"})
+(read-json "{\"width\": 300, \"height\": 200}")
+
 
